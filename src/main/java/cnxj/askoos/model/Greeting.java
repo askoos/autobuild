bfa25118bfcf16a2269389fcda1d3ea945e69442
askoos <askoos2032@sina.com>
2015-11-26 12:12:50 +0800

    第一次提交

---
+++ src/main/java/cnxj/askoos/model/Greeting.java
@@ -0,0 +1,24 @@
+package cnxj.askoos.model;
+
+/**
+ * Created by Askoos on 2015/11/26.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
