9ff093c1a7947fc327acc02c4bf08c42d4939e90
askoos <askoos2032@sina.com>
2015-11-26 12:50:09 +0800

    第一次提交

--- src/test/java/cnxj/askoos/AutobuildApplicationTests.groovy
+++ src/test/java/cnxj/askoos/AutobuildApplicationTests.groovy
@@ -6,6 +6,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
+/**
+ * Created by Askoos on 2015/11/26.
+ */
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = AutobuildApplication)
 @WebAppConfiguration
